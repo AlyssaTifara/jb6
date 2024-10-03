cdad7116425669753b7c2ebab406518dfb10eaa2
AlyssaTifara <tissaalyssa@gmail.com>
2024-10-03 12:16:00 +0700

    pk6

--- dosen.java
+++ dosen.java
@@ -5,17 +5,20 @@ import testpackage.Pegawai;
 public class Dosen extends Pegawai {
     public String nidn;
 
-    public Dosen() {
-        System.out.println(gaji);
+    public Dosen(String nip, String nama, double gaji, String nidn) {
         System.out.println("objek dari class dosen dibuat");
     }
 
+    public String getInfo(){
+        return "NIDN    : " + this.nidn + "\n";
+    }
+
     public String getAllInfo(){
-        String info = "";
-        info += "NIP    : " + super.nip + "\n";
-        info += "Nama   : " + super.nama + "\n";
-        info += "Gaji   : " + super.gaji + "\n";
-        info += "NIDN   : " + super.nidn + "\n";
+        String info = super.getInfo();
+        // info += "NIP    : " + super.nip + "\n";
+        // info += "Nama   : " + super.nama + "\n";
+        // info += "Gaji   : " + super.gaji + "\n";
+        info += "NIDN   : " + nidn + "\n";
         return info;
     }
 }
--- inheritanceDemo.java
+++ inheritanceDemo.java
@@ -1,12 +1,12 @@
 
 public class inheritanceDemo {
     public static void main(String[] args) {
-        Dosen dosen1 = new Dosen();
+        Dosen dosen1 = new Dosen("34329837","Yansy Ayuningtyas",3000000,"1989432439");
 
-        dosen1.nama = "Yansy Ayuningtyas";
-        dosen1.nip = "34329837";
-        dosen1.gaji = 3000000;
-        dosen1.nidn = "1989432439";
+        // dosen1.nama = "Yansy Ayuningtyas";
+        // dosen1s.nip = "34329837";
+        // dosen1.gaji = 3000000;
+        // dosen1.nidn = "1989432439";
 
         System.out.println(dosen1.getAllInfo());
     }
